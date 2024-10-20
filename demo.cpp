0e9908435d3e68e2040da7966a86ec16f73c5d1b
Meraj Sheikh <135554085+iameraj@users.noreply.github.com>
2024-10-20 14:04:57 +0000

    Update demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -1,5 +1,4 @@
 #include "device.c"
-#include <assert.h>
 #include <iostream>
 using namespace std;
 
@@ -20,24 +19,24 @@ int main(void)
         return 0;
 }
 
-void bresenham()
+void bresenham(void)
 {
         int x0, y0, x1, y1;
         cout << "Enter a pair of coordinates:(x1 y1 x2 y2): ";
         cin >> x0 >> y0 >> x1 >> y1;
         bool steep = false;
-        if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
-                std::swap(x0, y0);
-                std::swap(x1, y1);
+        if (abs(x0 - x1) < abs(y0 - y1)) {
+                swap(x0, y0);
+                swap(x1, y1);
                 steep = true;
         }
         if (x0 > x1) {
-                std::swap(x0, x1);
-                std::swap(y0, y1);
+                swap(x0, x1);
+                swap(y0, y1);
         }
         int dx      = x1 - x0;
         int dy      = y1 - y0;
-        int derror2 = std::abs(dy) * 2;
+        int derror2 = abs(dy) * 2;
         int error2  = 0;
         int y       = y0;
         for (int x = x0; x <= x1; x++) {
