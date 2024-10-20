5fd9dd520855e007ce8060cfdbaaccb2c95b6d9d
Meraj Sheikh <135554085+iameraj@users.noreply.github.com>
2024-10-20 14:03:31 +0000

    added demo file

---
+++ demo.cpp
@@ -0,0 +1,55 @@
+#include "device.c"
+#include <assert.h>
+#include <iostream>
+using namespace std;
+
+void bresenham(void);
+
+int main(void)
+{
+        clrscr();
+        int height = get_window_height();
+        int width  = get_window_width();
+
+        int n, x, y;
+        cout << "Device Dimensions: " << height << " x " << width << endl;
+        cout << "Number of lines to draw : ";
+        cin >> n;
+        for (int i = 0; i < n; i++) { bresenham(); }
+        getch();
+        return 0;
+}
+
+void bresenham()
+{
+        int x0, y0, x1, y1;
+        cout << "Enter a pair of coordinates:(x1 y1 x2 y2): ";
+        cin >> x0 >> y0 >> x1 >> y1;
+        bool steep = false;
+        if (std::abs(x0 - x1) < std::abs(y0 - y1)) {
+                std::swap(x0, y0);
+                std::swap(x1, y1);
+                steep = true;
+        }
+        if (x0 > x1) {
+                std::swap(x0, x1);
+                std::swap(y0, y1);
+        }
+        int dx      = x1 - x0;
+        int dy      = y1 - y0;
+        int derror2 = std::abs(dy) * 2;
+        int error2  = 0;
+        int y       = y0;
+        for (int x = x0; x <= x1; x++) {
+                if (steep) {
+                        put_pixel(y, x, '#');
+                } else {
+                        put_pixel(x, y, '#');
+                }
+                error2 += derror2;
+                if (error2 > dx) {
+                        y += (y1 > y0 ? 1 : -1);
+                        error2 -= dx * 2;
+                }
+        }
+}
